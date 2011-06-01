8cb2df1b3604ca65353330db45396fb647b03d91
Tomáš Čejka <tomas@kassoft.eu>
2011-06-01 19:15:45 +0200

    Bop::Translate attempt of completion of operations

--- mila/strom.cpp
+++ mila/strom.cpp
@@ -382,9 +382,33 @@ void Bop::Translate()
       emitRM("LDC",ac,1,ac,"true case") ;
       break;
     case NotEq:
+      emitRO("SUB",ac,ac1,ac,"op !=") ;
+      emitRM("JNE",ac,2,pc,"br if true") ;
+      emitRM("LDC",ac,0,ac,"false case") ;
+      emitRM("LDA",pc,1,pc,"unconditional jmp") ;
+      emitRM("LDC",ac,1,ac,"true case") ;
+      break;
     case Greater:
+      emitRO("SUB",ac,ac1,ac,"op !=") ;
+      emitRM("JGT",ac,2,pc,"br if true") ;
+      emitRM("LDC",ac,0,ac,"false case") ;
+      emitRM("LDA",pc,1,pc,"unconditional jmp") ;
+      emitRM("LDC",ac,1,ac,"true case") ;
+      break;
     case LessOrEq:
+      emitRO("SUB",ac,ac1,ac,"op !=") ;
+      emitRM("JLE",ac,2,pc,"br if true") ;
+      emitRM("LDC",ac,0,ac,"false case") ;
+      emitRM("LDA",pc,1,pc,"unconditional jmp") ;
+      emitRM("LDC",ac,1,ac,"true case") ;
+      break;
     case GreaterOrEq:
+      emitRO("SUB",ac,ac1,ac,"op !=") ;
+      emitRM("JGE",ac,2,pc,"br if true") ;
+      emitRM("LDC",ac,0,ac,"false case") ;
+      emitRM("LDA",pc,1,pc,"unconditional jmp") ;
+      emitRM("LDC",ac,1,ac,"true case") ;
+      break;
     default:
       //TODO
       emitComment("!!!!!BUG: Unknown operator!!!!!!");
